013d245c3f5f2b9379cdcd7b1225f55a629e3913
admin <admin@Mac-mini-admin.local>
2022-01-18 18:56:26 +0200

    fix

--- src/components/global-component/education-hero/EducationHero.styled.js
+++ src/components/global-component/education-hero/EducationHero.styled.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
+import {lg} from "../../../styles/utils/media_queries";
 
 
 
 export const EducationHeroStyles = styled.section` 
-
+    ${lg(`
+        h1{
+            font-size: 96px;
+        }
+    `)}
 `;
