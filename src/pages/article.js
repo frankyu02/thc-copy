be51caecddfbbf05fabc70f46dfcac8f3090a7cb
MatviienkoNataliia <admin@MacBook-Air-Nataliia.local>
2022-01-22 15:17:40 +0200

    created hero

--- src/pages/article.js
+++ src/pages/article.js
@@ -4,17 +4,24 @@ import {Footer} from '../components/footer/Footer';
 import Layout from "../components/layout/Layout";
 import {DoubleBanner} from "../components/article/double_banner/DoubleBanner";
 import {ShopLink} from "../components/ui/shop_link/ShopLink";
+import {EducationHero} from "../components/global_component/education-hero/EducationHero";
+
 
 const Article = () => {
     const seo = {
         title: 'Article',
         description: 'Article Description'
     }
+    const headingText = 'How to Judge the Quality of Cannabis';
+    const data = 'JANUARY 28, 2021';
+    const banner = '';
+
 
     return (
         <>
             <Layout seo={seo}>
                 <HeaderMenu/>
+                <EducationHero title={headingText} data={data} banner={banner}/>
                 <DoubleBanner/>
                 <Footer/>
                 <ShopLink/>
--- src/pages/education.js
+++ src/pages/education.js
@@ -1,26 +1,46 @@
 import * as React from "react";
 import { HeaderMenu } from "../components/header/HeaderMenu";
+import {graphql, useStaticQuery} from "gatsby";
 import {Footer} from '../components/footer/Footer';
 import Layout from "../components/layout/Layout";
-import {EducationHero} from "../components/global_component/education-hero/EducationHero";
+import {SmallHero} from "../components/global_component/small_hero/SmallHero";
+
 
 const EducationPage = () => {
 
-    const headingText = 'How to Judge the Quality of Cannabis';
-    const data = 'JANUARY 28, 2021';
-    const banner = '';
-
+    const data = useStaticQuery(graphql`
+        query {
+            wpPage(id: {eq: "cG9zdDoyOTI="}) {
+                education {
+                    educationBanner {
+                        fieldGroupName
+                        educationBannerTitle
+                        educationBannerImg {
+                            localFile {
+                                childImageSharp {
+                                gatsbyImageData
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `)
 
     const seo = {
         title: 'Education',
         description: 'Education Description'
     }
 
+    const title = data?.allWpPage?.nodes[0]?.education?.educationBanner?.educationBannerTitle;
+    const banner = data?.allWpPage?.nodes[0]?.education?.educationBanner?.educationBannerImg?.localFile?.childImageSharp?.gatsbyImageData;
+
     return (
         <>
             <Layout seo={seo}>
                 <HeaderMenu/>
-                <EducationHero title={headingText} data={data} banner={banner}/>
+                <SmallHero title={title} banner={banner}/>
                 <Footer/>
             </Layout>
         </>
