6748e8c61dcf0129ea49888f57777299c1203493
MatviienkoNataliia <admin@MacBook-Air-Nataliia.local>
2022-01-24 12:38:07 +0200

    create hero section

---
+++ src/pages/delivery.js
@@ -0,0 +1,48 @@
+import * as React from "react";
+import Layout from "../components/layout/Layout";
+import {graphql, useStaticQuery} from "gatsby";
+import {SmallHero} from "../components/global_component/small_hero/SmallHero";
+import {HeaderMenu} from "../components/header/HeaderMenu";
+import {Footer} from "../components/footer/Footer";
+
+const DeliveryPage = () => {
+
+    const seo = {
+        title: 'Delivery Title',
+        description: 'Delivery Description'
+    }
+
+    const data = useStaticQuery(graphql`
+        query {
+            allWpPage(filter: {id: {eq: "cG9zdDo0MjE="}}) {
+                nodes {
+                  delivery {
+                    deliveryBanner {
+                      deliveryBannerTitle
+                      deliveryBannerImg {
+                        localFile {
+                          childImageSharp {
+                            gatsbyImageData
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+            }
+        }
+    `)
+    const title = data?.allWpPage?.nodes[0]?.delivery?.deliveryBanner?.deliveryBannerTitle;
+    const banner = data?.allWpPage?.nodes[0]?.delivery?.deliveryBanner?.deliveryBannerImg?.localFile?.childImageSharp?.gatsbyImageData;
+    return (
+        <>
+            <Layout seo={seo}>
+                <HeaderMenu/>
+                <SmallHero title={title} banner={banner}/>
+                <Footer/>
+            </Layout>
+        </>
+    );
+}
+
+export default DeliveryPage;
