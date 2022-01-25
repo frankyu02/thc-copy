af421acac1b61e17719e930d4fbc648fb9cc6482
MatviienkoNataliia <admin@MacBook-Air-Nataliia.local>
2022-01-25 18:36:28 +0200

    change largeBanner

--- src/pages/delivery.js
+++ src/pages/delivery.js
@@ -7,6 +7,8 @@ import {Footer} from "../components/footer/Footer";
 import {TitleSubtext} from "../components/delivery/title_subtext/TitleSubtext";
 import {InfoBanner} from "../components/delivery/delivery-info-banner/InfoBanner";
 import {MapRegions} from "../components/delivery/map-regions/MapRegions";
+import {LargeBanner} from "../components/global_component/large_banner/LargeBanner";
+import {getImage, withArtDirection} from "gatsby-plugin-image";
 
 
 const DeliveryPage = () => {
@@ -31,15 +33,50 @@ const DeliveryPage = () => {
                     }
                   }
                 }
+                deliveryLargeOrderBanner {
+                  deliveryLargeOrderBannerTitle
+                  deliveryLargeOrderBannerText
+                  deliveryLargeOrderBannerImgMobile {
+                    localFile {
+                      childImageSharp {
+                        gatsbyImageData(quality: 100)
+                      }
+                    }
+                  }
+                  deliveryLargeOrderBannerImg {
+                    localFile {
+                      childImageSharp {
+                        gatsbyImageData(quality: 100)
+                      }
+                    }
+                  }
+                  deliveryLargeOrderBannerButton {
+                    url
+                    title
+                    target
+                  }
+                }
               }
             }
           }
         }
+        
     `)
 
     const title = data?.allWpPage?.nodes[0]?.delivery?.deliveryBanner?.deliveryBannerTitle;
     const banner = data?.allWpPage?.nodes[0]?.delivery?.deliveryBanner?.deliveryBannerImg?.localFile?.childImageSharp?.gatsbyImageData;
 
+    const titleLargeBanner = data?.allWpPage?.nodes[0]?.delivery?.deliveryLargeOrderBanner?.deliveryLargeOrderBannerTitle;
+    const textLargeBanner = data?.allWpPage?.nodes[0]?.delivery?.deliveryLargeOrderBanner?.deliveryLargeOrderBannerText;
+    const butonLargeBanner = data?.allWpPage?.nodes[0]?.delivery?.deliveryLargeOrderBanner?.deliveryLargeOrderBannerButton;
+    const largeBanner = data?.allWpPage?.nodes[0]?.delivery?.deliveryLargeOrderBanner;
+    const images = withArtDirection(getImage(largeBanner.deliveryLargeOrderBannerImgMobile?.localFile), [
+        {
+            media: "(min-width: 768px)",
+            image: getImage(largeBanner.deliveryLargeOrderBannerImg?.localFile),
+        },
+    ])
+    
     return (
         <>
             <Layout seo={seo}>
@@ -48,6 +85,7 @@ const DeliveryPage = () => {
                 <TitleSubtext/>
                 <InfoBanner/>
                 <MapRegions/>
+                <LargeBanner images={images} titleLargeBanner={titleLargeBanner} textLargeBanner={textLargeBanner} butonLargeBanner={butonLargeBanner}/>
                 <Footer/>
             </Layout>
         </>
