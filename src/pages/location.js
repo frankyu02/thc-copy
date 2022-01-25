6a1e38d092f74404dc53568065b51a73c705b78a
smart-ui <smart-ui@MacForMike.local>
2022-01-25 15:24:26 +0200

    small banner

---
+++ src/pages/location.js
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { HeaderMenu } from "../components/header/HeaderMenu";
+import {Footer} from '../components/footer/Footer';
+import Layout from "../components/layout/Layout";
+import {ShopLink} from "../components/ui/shop_link/ShopLink";
+import {SmallHero} from "../components/global_component/small_hero/SmallHero";
+import {graphql, useStaticQuery} from "gatsby";
+
+const Location = () => {
+    const seo = {
+        title: 'Location',
+        description: 'Location Description'
+    }
+    const data = useStaticQuery(graphql`
+        query {
+            allWpPage(filter: {id: {eq: "cG9zdDo2NDM="}}) {
+                nodes {
+                    title
+                    location {
+                        locationBanner {
+                            locationBannerImg {
+                                localFile {
+                                    childImageSharp {
+                                        gatsbyImageData(quality: 100)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+    `)
+    const location = data?.allWpPage?.nodes[0];
+    const locationImg = data?.allWpPage?.nodes[0]?.location?.locationBanner;
+    return (
+        <>
+            <Layout seo={seo}>
+                <HeaderMenu/>
+                <SmallHero banner={locationImg?.locationBannerImg?.localFile?.childImageSharp?.gatsbyImageData} title={location?.title}/>
+                <Footer/>
+                <ShopLink/>
+            </Layout>
+        </>
+    )
+}
+
+export default Location
