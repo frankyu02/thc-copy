77f673551dc1b6a404de39c54a59808b9c08d2a1
Developer <you@example.com>
2022-01-28 10:33:46 +0200

    fix/age-gate

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,22 +4,22 @@ import GlobalStyles from "../../styles/GlobalStyles"
 import Typography from "../../styles/Typography"
 import { HeaderMenu } from "../header/HeaderMenu"
 import { Footer } from "../footer/Footer"
+import { AgeGate } from "../AgeGate/AgeGate";
 
 
 const Layout = ({ children }) => {
   return (
     <>
-
       <GlobalStyles />
       <Typography />
+      <AgeGate/>
       <HeaderMenu />
       <main>
         {children}
       </main>
       <Footer />
-
-
     </>
   )
 }
-export default Layout
+
+export default Layout;
