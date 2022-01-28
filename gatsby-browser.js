b0b1a88e1fb7d040d5e2b065394eeca910a7cf99
Developer <you@example.com>
2022-01-28 09:58:12 +0200

    merge feature/age-gate into development

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Wrapper from './gatsby-render-layout';
 
 export function wrapPageElement({element, props}) {
-    return element;
     return (
         <Wrapper {...props} element={element}/>
     )
