5fdec76bbb5a50dc96d0cdcfe9fe033073fdfdfd
viktormoskalev07 <viktormoskalev07@gmail.com>
2022-01-31 20:06:00 +0300

    remove items for test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
 import * as React from "react"
 import { HeroHome } from "../components/home/Hero"
 import { Legendary } from "../components/home/legendary/Legendary"
-import { ThcTv } from "../components/global_component/thc-tv/ThcTv"
-import { CategorySection } from "../components/home/category-section/CategorySection"
 import { Brands } from "../components/home/brands/Brands"
-import { SettingStandart } from "../components/home/setting_standart/SettingStandart"
-import { BlogBanner } from "../components/home/blog-banner/BlogBanner"
-import { MerchCarousel } from "../components/home/merch_carousel/MerchCarousel"
 import Seo from "../components/layout/Seo"
 import { ShopLink } from "../components/ui/shop_link/ShopLink"
-import {graphql, useStaticQuery} from "gatsby";
+import { graphql, useStaticQuery } from "gatsby"
 
 const HomePage = () => {
   const data = useStaticQuery(graphql`
@@ -28,7 +23,7 @@ const HomePage = () => {
     }
   `)
 
-  const seoData = data?.allWpPage?.nodes[0]?.template?.seoMetaTags;
+  const seoData = data?.allWpPage?.nodes[0]?.template?.seoMetaTags
   const seo = {
     title: seoData?.seoMetaTagsTitle,
     description: seoData?.seoMetaTagsDescription
@@ -38,11 +33,11 @@ const HomePage = () => {
     <Seo {...seo} />
     <HeroHome />
     <Legendary />
-    <MerchCarousel />
-    <CategorySection />
-    <ThcTv />
-    <BlogBanner />
-    <SettingStandart />
+    {/*<MerchCarousel />*/}
+    {/*<CategorySection />*/}
+    {/*<ThcTv />*/}
+    {/*<BlogBanner />*/}
+    {/*<SettingStandart />*/}
     <Brands />
     <ShopLink />
   </>
