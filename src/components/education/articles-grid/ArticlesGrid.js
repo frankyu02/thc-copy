0399e604aeb5e7d5999ce1291a4556eeaa7ff0fa
Developer <you@example.com>
2022-01-31 13:55:20 +0200

    done feature/education-collection-page

--- src/components/education/articles-grid/ArticlesGrid.js
+++ src/components/education/articles-grid/ArticlesGrid.js
@@ -1,63 +1,62 @@
-import * as React from "react";
+import React from "react";
+import { useStaticQuery, graphql, Link } from "gatsby";
 import { ArticlesGridStyles } from "./ArticlesGrid.styled";
+import { getImageData } from "../../../utils/get_image_data";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 export const ArticlesGrid = () => {
     
-    const title = "What is CBD Oil?";
     const button = "Read more";
     const img = "www";
+
+    const data = useStaticQuery(graphql`
+      query {
+        allWpEducation {
+          edges {
+            node {
+              slug
+              title
+              featuredImage {
+                node {
+                  localFile {
+                    childImageSharp {
+                      gatsbyImageData
+                    }
+                  }
+                }
+              }
+              id
+            }
+          }
+        }
+      }
+    `)
+
+    const educations = data?.allWpEducation?.edges;
 
     return (
         <ArticlesGridStyles>
             <div className="container">
               <div className="article-wrap">
-                <div className="article-item">
-                  <div className="article-img">
-                    <img src="/static/de678efaec14b60c18c417abc8880597/82da7/cards_join_bg_img_1.jpg" alt="article img" />
-                  </div>
-                  <h3>{title}</h3>
-                  <a href="#" className="article-buttom">{button}
-                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M1.65306 16.3469L17 1M17 1V17M17 1H1" stroke="white" strokeWidth="2"/>
-                    </svg>
-                  </a>
-                </div>
-
-                <div className="article-item">
-                  <div className="article-img">
-                  <img src="/static/de678efaec14b60c18c417abc8880597/82da7/cards_join_bg_img_1.jpg" alt="article img" />
-                  </div>
-                  <h3>{title}</h3>
-                  <a href="#" className="article-buttom">{button}
-                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M1.65306 16.3469L17 1M17 1V17M17 1H1" stroke="white" strokeWidth="2"/>
-                    </svg>
-                  </a>
-                </div>
-
-                <div className="article-item">
-                  <div className="article-img">
-                  <img src="/static/de678efaec14b60c18c417abc8880597/82da7/cards_join_bg_img_1.jpg" alt="article img" />
-                  </div>
-                  <h3>{title}</h3>
-                  <a href="#" className="article-buttom">{button}
-                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M1.65306 16.3469L17 1M17 1V17M17 1H1" stroke="white" strokeWidth="2"/>
-                    </svg>
-                  </a>
-                </div>
-
-                <div className="article-item">
-                  <div className="article-img">
-                  <img src="/static/de678efaec14b60c18c417abc8880597/82da7/cards_join_bg_img_1.jpg" alt="article img" />
-                  </div>
-                  <h3>{title}</h3>
-                  <a href="#" className="article-buttom">{button}
-                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M1.65306 16.3469L17 1M17 1V17M17 1H1" stroke="white" strokeWidth="2"/>
-                    </svg>
-                  </a>
-                </div>
+                {
+                  educations.map(item => {
+                    return (
+                      <div className="article-item" key={item?.node?.id}>
+                        <div className="article-img">
+                          <GatsbyImage image={getImageData(item?.node?.featuredImage.node)} alt={item?.node?.title} />
+                        </div>
+                        <h3>{item?.node?.title}</h3>
+                        <Link to={item?.node?.slug} className="article-buttom article-buttom-desktop">
+                          Read more
+                          <svg width="16" height="16" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0.734694 18.2653L18 1M18 1V19M18 1H0" stroke="#fff" strokeWidth="2" />
+                          </svg>
+                        </Link>
+                        
+                      </div>
+                    )
+                  })
+                }
               </div>
             </div>
         </ArticlesGridStyles>
