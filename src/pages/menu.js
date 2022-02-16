280977a7c0d4c82b2f60781cf6a7133cc7ac1ef2
LukeVibes <daschko.l@gmail.com>
2022-02-16 15:51:35 -0500

    trying to fix SSR bug 'document not available'

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,10 +1,9 @@
-import React, {useEffect, useReducer} from 'react';
+import React from 'react';
 import { QueryParamProvider } from 'use-query-params'
 import MenuHubApollo from '../components/menu/MenuHubApollo';
 import history from '../utils/history';
 
 export default function menu(){
-   
     return(
         <QueryParamProvider history={history}>
             <MenuHubApollo history={history}/>
