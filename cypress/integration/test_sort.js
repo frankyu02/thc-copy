dff2b0ebc7945b6aacd87ff49803b02ec4e42c8b
frankyu02 <frankyu02@yahoo.ca>
2022-02-24 12:55:47 -0500

    sort test created

---
+++ cypress/integration/test_sort.js
@@ -0,0 +1,10 @@
+describe('Test Categories', () => {
+    before(() => {
+        cy.setLocalStorage("accessTime", 1645033041559)
+    })
+    it('Navigates through menu Brands', () => {
+        cy.visit("/menu")
+        cy.get('.filtersButton').click()
+        cy.contains('sort')
+    })
+})
