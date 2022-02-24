8a5ccb90004dd5aec1ff0eb9cf170232317341c7
frankyu02 <frankyu02@yahoo.ca>
2022-02-24 11:04:46 -0500

    category tests added

--- cypress/integration/test_category_mobile.js
+++ cypress/integration/test_category_mobile.js
@@ -7,6 +7,26 @@ describe('Test Categories', () => {
         cy.setLocalStorage("accessTime", 1645033041559)
     })
     it('Navigates through menu categories', () => {
-        
+        cy.visit("/menu")
+        cy.get('.filtersButton').click()
+        //click category from menu page
+        cy.contains('edibles').click()
+        //check categories gone, subcategories present
+        cy.get('.categories').should('not.contain', 'edibles').should('not.contain', 'flowers').should('not.contain', 'flowers')
+        .should('not.contain', 'pre rolls').should('not.contain', 'vaporizers').should('not.contain', 'concentrates').should('not.contain', 'tinctures')
+        .should('not.contain', 'topicals').should('contain', 'baked goods').should('contain', 'drinks').should('contain', 'chocolates')
+        .should('contain', 'gummies').should('contain', 'capsules tablets').should('contain', 'dissolvables')
+        //check weight update
+        cy.contains('Format').click()
+        cy.get('.weights').should('not.contain', '400g')
+        cy.url().should('eq', 'http://localhost:8000/menu?category=EDIBLES')
+        //sub category traversal test
+        cy.get('.categories').contains('chocolates').click()
+        cy.url().should('eq', 'http://localhost:8000/menu?category=EDIBLES&subcategory=CHOCOLATES')
+        cy.get('.categories').contains('gummies').click()
+        cy.url().should('eq', 'http://localhost:8000/menu?category=EDIBLES&subcategory=GUMMIES')
+        //click category from page 3
+        cy.visit('/menu')
+        //click category from page 37
     })
 })
