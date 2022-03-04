6dd53072bbdeb6b1013aa153695a8691e1a73ab1
frankyu02 <frankyu02@yahoo.ca>
2022-03-04 13:55:26 -0500

    THC slider testing created

--- cypress/integration/Cypress Tests/mainTest.js
+++ cypress/integration/Cypress Tests/mainTest.js
@@ -1,5 +1,8 @@
 import TestSlider from "./testSlider";
-
+export const SliderRange = {
+    min: 0,
+    max: 50
+}
 export default function MainTest(){
     TestSlider;
 }
--- cypress/integration/Cypress Tests/testSlider.js
+++ cypress/integration/Cypress Tests/testSlider.js
@@ -1,4 +1,5 @@
 import {accessTime} from '../variable';
+import { SliderRange } from './mainTest';
     describe('Test the THC and CBD Sliders', () => {
         before(() => {
             // log in only once before any of the tests run.
@@ -7,12 +8,51 @@ import {accessTime} from '../variable';
             // in your Resources -> Cookies panel in the Chrome Dev Tools.
             cy.setLocalStorage("accessTime", accessTime)
         })
-        it('Inputs values into slider box', () => {
+        it('tests setting THC max value', () => {
             //maxTHC value
-            cy.log('setMaxTHC')
             cy.visit("/menu")
             cy.get('[data-cy="menuGrid"]', {timeout: 60000})
+            let val;
+            cy.get('[data-cy=ProductCount]').then(($count) => { //This code gets the product count
+                val = $count.text()
+            })
+            let input = Math.floor((Math.random() * ((SliderRange.max - 1) - SliderRange.min)) + SliderRange.min);
             cy.get('[data-cy=filtersButton]').click()
-            cy.get('[data-cy=THC-right]').type('{backspace}{backspace}'+ Math.floor((Math.random() * (50 - 0)) + 0))
+            cy.get('[data-cy=THC-right]').type('{backspace}{backspace}'+ input +'{enter}')
+            cy.get('[data-cy="clearAll"]', {timeout: 60000}) //checks that the filter's applied
+            cy.get('[data-cy="menuGrid"]', {timeout: 60000}) //checks that the menu is updated
+            cy.get('[data-cy=ProductCount]').then(($count) => { //This code gets the updated product count
+                expect($count.text()).not.to.eq(val)
+            })
+            //manually go to filterd page using URl
+            cy.url().then(($url)=>{
+                cy.visit($url)
+                cy.get('[data-cy=filtersButton]').click()
+                cy.get('[data-cy=THC-right]').should('have.value', input)
+            })
+        })
+        
+        it('tests setting THC min value', ()=>{
+            cy.setLocalStorage("accessTime", accessTime)
+            cy.visit("/menu")
+            cy.get('[data-cy="menuGrid"]', {timeout: 60000})
+            let val;
+            cy.get('[data-cy=ProductCount]').then(($count) => { //This code gets the product count
+                val = $count.text()
+            })
+            let input = Math.floor((Math.random() * ((SliderRange.max) - (SliderRange.min + 1))) + (SliderRange.min + 1));
+            cy.get('[data-cy=filtersButton]').click()
+            cy.get('[data-cy=THC-left]').type('{backspace}{backspace}'+ input +'{enter}')
+            cy.get('[data-cy="clearAll"]', {timeout: 60000}) //checks that the filter's applied
+            cy.get('[data-cy="menuGrid"]', {timeout: 60000}) //checks that the menu is updated
+            cy.get('[data-cy=ProductCount]').then(($count) => { //This code gets the updated product count
+                expect($count.text()).not.to.eq(val)
+            })
+            //manually go to filterd page using URl
+            cy.url().then(($url)=>{
+                cy.visit($url)
+                cy.get('[data-cy=filtersButton]').click()
+                cy.get('[data-cy=THC-left]').should('have.value', input)
+            })
         })
     })
