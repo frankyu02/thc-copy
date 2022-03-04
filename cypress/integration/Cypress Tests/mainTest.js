9aa09b841a58def21fcaca0ccf127ee6629dfbba
frankyu02 <frankyu02@yahoo.ca>
2022-03-04 14:05:48 -0500

    pagination test added to main test

--- cypress/integration/Cypress Tests/mainTest.js
+++ cypress/integration/Cypress Tests/mainTest.js
@@ -1,8 +1,10 @@
 import TestSlider from "./testSlider";
+import TestPagination from './test_pagination';
 export const SliderRange = {
     min: 0,
     max: 50
 }
 export default function MainTest(){
     TestSlider;
+    TestPagination;
 }
