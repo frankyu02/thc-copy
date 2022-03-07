8259e626da3bba028a758d1761ddbce1079c05ac
LukeVibes <daschko.l@gmail.com>
2022-03-07 15:51:13 -0500

    seo menu

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { graphql, useStaticQuery } from "gatsby"
 import MenuHubApollo from '../components/menu/MenuHubApollo';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Seo from "../components/layout/Seo"
 
-export default function menu(){
+export default function Menu(){
+    const data = useStaticQuery(graphql`
+        query {
+            allWpPage(filter: {slug: {eq: "menu"}}) {
+                nodes {
+                    template {
+                        seoMetaTags {
+                            seoMetaTagsDescription
+                            seoMetaTagsJsonShema
+                            seoMetaTagsTitle
+                        }
+                    }
+                }
+            }
+        }
+    `)
+    const seoData = data?.allWpPage?.nodes[0]?.template?.seoMetaTags
+    const seo = {
+        title: seoData?.seoMetaTagsTitle,
+        description: seoData?.seoMetaTagsDescription
+    }
     return(
         <>
+        <Seo {...seo} />
         <MenuHubApollo />
         </>
     )
