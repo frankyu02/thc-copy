3abf9039bd072aa632adc532491369ad9b37d4aa
LukeVibes <daschko.l@gmail.com>
2022-03-11 13:11:38 -0500

    added sitemap- custom example from gatsby+wp, remove /products

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,7 @@ module.exports = {
             return { ...page, ...wpNodeMap[page.path] }
           })
         },
+        excludes: ["/products"],
         serialize: ({ path, modifiedGmt }) => {
           return {
             url: path,
